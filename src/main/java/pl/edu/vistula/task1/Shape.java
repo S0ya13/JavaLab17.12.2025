5018410d5556d03c4034b0500621633ee3b77b89
Soya <oj.savchak@gmail.com>
2025-12-17 17:07:45 +0100

    First task done.

---
+++ src/main/java/pl/edu/vistula/task1/Shape.java
@@ -0,0 +1,12 @@
+package pl.edu.vistula.task1;
+
+public enum Shape {
+    SQUARE,
+    RECTANGLE,
+    CIRCLE,
+    TRAPEZOID,
+    RHOMBUS,
+    TRIANGLE,
+    OVAL,
+    HEXAGON
+}
---
+++ src/main/java/pl/edu/vistula/task1/ShapeMain.java
@@ -0,0 +1,9 @@
+package pl.edu.vistula.task1;
+
+public class ShapeMain {
+    public static void main(String[] args) {
+        for (Shape shape : Shape.values()) {
+            System.out.println(shape.name() + " -> ordinal: " + shape.ordinal());
+        }
+    }
+}
